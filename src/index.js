a6e33be835ccf6e3b90c19d92519b0b8bcbdbcab
bamboo_shoot <156806318+kon65138@users.noreply.github.com>
2025-02-08 14:20:58 +0000

    add function that returns relevent info from vc data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
+import { hu } from 'date-fns/locale';
 import './style.css';
 
 async function fetchWeatherData(location = 'london', unitGroup = 'uk') {
-  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unitGroup}&key=PT7JZF8VMTU9ZYHQWDWUQX3QS`;
-  const response = await fetch(url, { mode: 'cors' });
-  const weatherData = await response.json();
-  console.log(weatherData);
+  try {
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unitGroup}&key=PT7JZF8VMTU9ZYHQWDWUQX3QS`;
+    const response = await fetch(url, { mode: 'cors' });
+    const weatherData = await response.json();
+    const processedWeatherData = processWeatherData(weatherData);
+    console.log(processedWeatherData);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-fetchWeatherData();
+function processWeatherData(weatherData) {
+  const resolvedAddress = weatherData.resolvedAddress;
+  const temperature = weatherData.currentConditions.temp;
+  const time = weatherData.currentConditions.datetime;
+  const humidity = weatherData.currentConditions.humidity;
+  const icon = weatherData.currentConditions.icon;
+  const conditions = weatherData.currentConditions.conditions;
+  const description = weatherData.description;
+  const windspeed = weatherData.currentConditions.windspeed;
+
+  return [
+    resolvedAddress,
+    temperature,
+    time,
+    humidity,
+    icon,
+    conditions,
+    description,
+    windspeed,
+  ];
+}
+
+fetchWeatherData('wokingham');
