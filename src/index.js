1babcc280a3253c5f090a2f1a57ba740dedc413e
bamboo_shoot <156806318+kon65138@users.noreply.github.com>
2025-02-11 11:30:59 +0000

    add event lister to searchbtn console log results

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
 import './style.css';
+import clear_day from './imgs/weatherIcons/clear-day.svg';
+import clear_night from './imgs/weatherIcons/clear-night.svg';
+import cloudy from './imgs/weatherIcons/cloudy.svg';
+import fog from './imgs/weatherIcons/fog.svg';
+import hail from './imgs/weatherIcons/hail.svg';
+import partly_cloudy_day from './imgs/weatherIcons/partly-cloudy-day.svg';
+import partly_cloudy_night from './imgs/weatherIcons/partly-cloudy-night.svg';
+import rain_snow_showers_day from './imgs/weatherIcons/rain-snow-showers-day.svg';
+import rain_snow_showers_night from './imgs/weatherIcons/rain-snow-showers-night.svg';
+import rain_snow from './imgs/weatherIcons/rain-snow.svg';
+import rain from './imgs/weatherIcons/rain.svg';
+import showers_day from './imgs/weatherIcons/showers-day.svg';
+import showers_night from './imgs/weatherIcons/showers-night.svg';
+import sleet from './imgs/weatherIcons/sleet.svg';
+import snow_showers_day from './imgs/weatherIcons/snow-showers-day.svg';
+import snow_showers_night from './imgs/weatherIcons/snow-showers-night.svg';
+import snow from './imgs/weatherIcons/snow.svg';
+import thunder_rain from './imgs/weatherIcons/thunder-rain.svg';
+import thunder_showers_day from './imgs/weatherIcons/thunder-showers-day.svg';
+import thunder_showers_night from './imgs/weatherIcons/thunder-showers-night.svg';
+import thunder from './imgs/weatherIcons/thunder.svg';
+import wind from './imgs/weatherIcons/wind.svg';
+
+const searchBtn = document.getElementById('searchBtn');
+
+searchBtn.addEventListener('click', () => {
+  const searchInput = document.getElementById('search');
+  const searchValue = searchInput.value;
+  fetchWeatherData(searchValue);
+});
 
 async function fetchWeatherData(location = 'london', unitGroup = 'uk') {
   try {
@@ -36,5 +66,3 @@ function processWeatherData(weatherData) {
     precipitationProbability: precipitation,
   };
 }
-
-fetchWeatherData('afghanistan');
