3213975d893517c325b927283072aef026282120
bamboo_shoot <156806318+kon65138@users.noreply.github.com>
2025-02-11 13:42:49 +0000

    add html functionality

--- src/index.js
+++ src/index.js
@@ -22,6 +22,31 @@ import thunder_showers_night from './imgs/weatherIcons/thunder-showers-night.svg
 import thunder from './imgs/weatherIcons/thunder.svg';
 import wind from './imgs/weatherIcons/wind.svg';
 
+const iconSvgs = new Map([
+  ['clear_day', clear_day],
+  ['clear_night', clear_night],
+  ['cloudy', cloudy],
+  ['fog', fog],
+  ['hail', hail],
+  ['partly_cloudy_day', partly_cloudy_day],
+  ['partly_cloudy_night', partly_cloudy_night],
+  ['rain_snow_showers_day', rain_snow_showers_day],
+  ['rain_snow_showers_night', rain_snow_showers_night],
+  ['rain_snow', rain_snow],
+  ['rain', rain],
+  ['showers_day', showers_day],
+  ['showers_night', showers_night],
+  ['sleet', sleet],
+  ['snow_showers_day', snow_showers_day],
+  ['snow_showers_night', snow_showers_night],
+  ['snow', snow],
+  ['thunder_rain', thunder_rain],
+  ['thunder_showers_day', thunder_showers_day],
+  ['thunder_showers_night', thunder_showers_night],
+  ['thunder', thunder],
+  ['wind', wind],
+]);
+
 const searchBtn = document.getElementById('searchBtn');
 
 searchBtn.addEventListener('click', () => {
@@ -34,11 +59,27 @@ async function fetchWeatherData(location = 'london', unitGroup = 'uk') {
   try {
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unitGroup}&key=PT7JZF8VMTU9ZYHQWDWUQX3QS`;
     const response = await fetch(url, { mode: 'cors' });
+    if (!response.ok) {
+      if (response.status === 400) {
+        throw new Error('invalid request (no location found)');
+      } else if (response.status === 401) {
+        throw new Error('api key unauthorized');
+      } else if (response.status === 429) {
+        throw new Error('too many requests');
+      } else if (response.status === 500) {
+        throw new Error('api server error');
+      }
+    } else {
+      const errorSpan = document.querySelector('.error');
+      errorSpan.textContent = '';
+    }
     const weatherData = await response.json();
-    const processedWeatherData = processWeatherData(weatherData);
-    console.log(processedWeatherData);
     console.log(weatherData);
+    const processedWeatherData = processWeatherData(weatherData);
+    updateHtml(processedWeatherData);
   } catch (error) {
+    const errorSpan = document.querySelector('.error');
+    errorSpan.textContent = error;
     console.error(error);
   }
 }
@@ -48,7 +89,7 @@ function processWeatherData(weatherData) {
   const temperature = weatherData.currentConditions.temp;
   const time = weatherData.currentConditions.datetime;
   const humidity = weatherData.currentConditions.humidity;
-  const icon = weatherData.currentConditions.icon;
+  const icon = weatherData.currentConditions.icon.replaceAll('-', '_');
   const conditions = weatherData.currentConditions.conditions;
   const description = weatherData.description;
   const windspeed = weatherData.currentConditions.windspeed;
@@ -65,4 +106,29 @@ function processWeatherData(weatherData) {
     currentWindSpeed: windspeed,
     precipitationProbability: precipitation,
   };
+}
+
+function updateHtml(data) {
+  const location = document.querySelector('.location');
+  const time = document.querySelector('.time');
+  const icon = document.getElementById('iconSvg');
+  const temperature = document.querySelector('.temp');
+  const conditions = document.querySelector('.conditions');
+  const humidity = document.querySelector('.humidity .value');
+  const windspeed = document.querySelector('.windspeed .value');
+  const precipitation = document.querySelector('.precipitation .value');
+  const description = document.querySelector('.generalOverview');
+  const weatherContentDiv = document.querySelector('.weatherContent');
+
+  location.textContent = data.address;
+  time.textContent = `time of report: ${data.currentTime} (local time)`;
+  icon.setAttribute('src', iconSvgs.get(data.weatherIcon));
+  temperature.textContent = `${data.temp}°c`;
+  conditions.textContent = data.currentCondition;
+  humidity.textContent = `${data.currentHumidity}%`;
+  windspeed.textContent = `${data.currentWindSpeed}mph`;
+  precipitation.textContent = `${data.precipitationProbability}%`;
+  description.textContent = data.overallDesciption;
+
+  weatherContentDiv.style.display = 'flex';
 }
